6945336c0c07e4865811af1821051bd1b2339679
Krish Vira <krish.vira@nmims.in>
2025-04-17 16:23:32 +0530

    Return role fields in current user details route

--- backend/src/app/blueprints/user/current.py
+++ backend/src/app/blueprints/user/current.py
@@ -5,7 +5,6 @@ from quart import Blueprint
 from quart_schema import validate_response
 
 from ...data_model import User
-from . import UserDetails
 from .authentication import ensure_authenticated, current_user
 
 
@@ -17,16 +16,22 @@ class UserDetailsWithoutPasswordHash:
     username: str
     full_name: str
     email: str
+    test_setter_role: bool
+    test_taker_role: bool
+    invigilator_role: bool
 
     @classmethod
-    def from_structural_superset(cls, user: User | UserDetails) -> Self:
+    async def from_structural_superset(cls, user: User) -> Self:
         return cls(
             username=user.username,
             full_name=user.full_name,
-            email=user.email
+            email=user.email,
+            test_setter_role=await user.awaitable_attrs.test_setter_role is not None,
+            test_taker_role=await user.awaitable_attrs.test_taker_role is not None,
+            invigilator_role=await user.awaitable_attrs.invigilator_role is not None
         )
 
 @bp.get('/details') # type: ignore
 @validate_response(UserDetailsWithoutPasswordHash)
 async def get_details() -> UserDetailsWithoutPasswordHash:
-    return UserDetailsWithoutPasswordHash.from_structural_superset(current_user)
+    return await UserDetailsWithoutPasswordHash.from_structural_superset(current_user)
